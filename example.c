fe9edb299d292ab932389c3b66614353e35fe15c
Pengu MC <michiel.van.der.coelen@gmail.com>
2018-08-09 15:44:16 +0200

    Add some comments

--- example.c
+++ example.c
@@ -3,33 +3,40 @@
 #include <stdint.h>
 #include "ATtiny_hardware_serial.h"
 
+// Example file. On startup 16 bytes will be sent.
+// After that everything you send is echoed back
+// Baudrate is 10000.
+// Buffers are USI_BUFFER_SIZE bytes large. (30)
+
+// You need this global variable since we need it in the interrupt routines.
 struct serial_handle ser;
 
-uint8_t reverse_byte(uint8_t n) {
-    int i, temp;
-    temp = 0;
-    for (i = 0; i < 8; ++i) {
-        temp <<= 1;
-        temp |= 0x01 & n;
-        n >>= 1;
-    }
-    return temp;
-}
-
-
 int main() {
-    DDRB |= (1<<PB4);
+    // Call the initializer to setup the ports (PB0 and PB1) and registers.
     ATtiny_serial_init();
+    // enable interrupts.
     sei();
 
+    // Wait a bit
     int i;
     for (i  =0; i < 10000; ++i) {;}
+    // Setup the first message 
     for (i = 0; i < USI_BUF_SIZE; ++i) {
         ser.write_buf[i] = 1 << (i % 8);
     }
-
+    // Start sending it.
+    // This function returns as soon as the sequence has started
+    // Interrupts will handle the rest.
     ATtiny_serial_write_async(16);
+
+    // You can check ser.write_index or ser.usi_mode if you want to know 
+    // When it's done.
+
     while(1) {
+        // ser.read_flag will be 1 when something has been received. 
+        // ser.read_flag can be cleared by hand, it will also be cleared when a new message is being received.
+        // ser.read_index will be the number of bytes received.
+        // The bytes are in ser.read_buffer
         if (ser.read_flag != 0) {
             for (i = 0; i < ser.read_index; ++i) {
                 ser.write_buf[i] = ser.read_buf[i];
